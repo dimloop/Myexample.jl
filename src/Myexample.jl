e01583e5de1b0eaf69df0bdbf0322845146fd4fd
dimloop <dimitris.loop@gmail.com>
2022-11-17 13:21:42 +0200

    this is a linear function

--- src/Myexample.jl
+++ src/Myexample.jl
@@ -1,5 +1,7 @@
 module Myexample
 
-# Write your package code here.
+include("extra_file.jl")
+
+export my_f
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using Myexample
 using Test
 
+
+
 @testset "Myexample.jl" begin
-    # Write your tests here.
+    #want 2x+3y 
+    @test my_f(2,1) == 7
+    @test my_f(2,3) == 13
 end
